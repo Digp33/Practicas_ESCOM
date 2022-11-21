f4b2fbebdd429ef9ea3b271a93b34cd3043ee2d6
Digp33 <85522284+Digp33@users.noreply.github.com>
2022-11-21 12:56:42 -0600

    Mejoramiento de Matrices1.c
    Se crearon las funciones  leer_matriz e imprimir_matriz

--- matrices1.c
+++ matrices1.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #define N 10
 #define M 10
+void leer_matriz(int A[N][M], int F, int C);
+void imprimir_matriz(int A[N][M],int F,int C);
+void leer_matriz(int A[N][M], int F, int C)
+{
+	int i,j;
+	for(i=0;i<F;i++)
+	{
+		for (j = 0;j<C;j++)
+		{
+			printf("Ingresa el valor de [%d][%d]\n",i,j );
+			scanf("%d",&A[i][j]);
+			//fflush(stdin);
+		}
+	}
+}
+
+void imprimir_matriz(int A[N][M],int F,int C)
+{
+	int i,j;
+	for(i=0;i<F;i++)
+	{
+		for (j = 0; j < C; j++)
+		{
+			printf("%d\t",A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 int main(int argc, char *argv[]) {
 	int F,C, i,j;
 	int mac1[N][M],mac2[N][M];
@@ -10,27 +40,10 @@ int main(int argc, char *argv[]) {
 	scanf("%d",&F);
 	printf("ingrese el numero de las columnas\n");
 	scanf("%d",&C);
-
 	printf("Primer matriz\n");
-	for(i=0;i<F;i++)
-	{
-		for (j = 0;j<C;j++)
-		{
-			printf("Ingresa el valor de [%d][%d]\n",i,j );
-			scanf("%d",&mac1[i][j]);
-			//fflush(stdin);
-		}
-	}
+	leer_matriz(mac1,F,C);
 	printf("Segunda matriz\n");
-	for(i=0;i<F;i++)
-	{
-		for (j = 0;j<C;j++)
-		{
-			printf("Ingresa el valor de [%d][%d]\n",i,j );
-			scanf("%d",&mac2[i][j]);
-			//fflush(stdin);
-		}
-	}
+	leer_matriz(mac2,F,C);
 	for(i=0;i<F;i++)
 	{
 		for (j = 0; j < C; j++)
@@ -38,18 +51,12 @@ int main(int argc, char *argv[]) {
 			macR[i][j]=mac1[i][j]+mac2[i][j];
 		}
 	}
+	printf("La matriz uno es:\n");
+	imprimir_matriz(mac1,F,C);
+	printf("La matriz dos es: \n");
+	imprimir_matriz(mac2,F,C);
 	printf("Matriz resultado\n");
-	for(i=0;i<F;i++)
-	{
-		for (j = 0; j < C; j++)
-		{
-			//printf("Ingresa el valor de [%i][%i]\n",i++,j++ );
-			//scanf("%i",&macR[i][j]);
-			printf("%d\t",macR[i][j]);
-		}
-		printf("\n");
-	}
-
+	imprimir_matriz(macR,F,C);
 
 	return 0;
 }
